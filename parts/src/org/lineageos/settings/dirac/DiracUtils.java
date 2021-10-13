a7983012dcd024a6ba84f7525ae5954098e9d308
rajadeja <rajsjadeja4@gmail.com>
2021-10-13 20:39:00 +0200

    Revert "sweet: parts: Dont restore values on boot"
    This reverts commit f50b57043545a79a7af475ac68aba53e26cc871a.

--- parts/src/org/lineageos/settings/dirac/DiracUtils.java
+++ parts/src/org/lineageos/settings/dirac/DiracUtils.java
@@ -33,6 +33,9 @@ public final class DiracUtils {
             mInitialized = true;
             mContext = context;
             mDiracSound = new DiracSound(0, 0);
+            mDiracSound.setMusic(mDiracSound.getMusic());
+            mDiracSound.setHeadsetType(mDiracSound.getHeadsetType());
+            setLevel(getLevel());
         }
     }
 
@@ -50,6 +53,16 @@ public final class DiracUtils {
         for (int band = 0; band <= level.length - 1; band++) {
             mDiracSound.setLevel(band, Float.valueOf(level[band]));
         }
+    }
+
+    protected static String getLevel() {
+        String selected = "";
+        for (int band = 0; band <= 6; band++) {
+            int temp = (int) mDiracSound.getLevel(band);
+            selected += String.valueOf(temp);
+            if (band != 6) selected += ",";
+        }
+        return selected;
     }
 
     protected static void setHeadsetType(int paramInt) {
